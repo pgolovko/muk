b6246daeb264697c1812e8790eba94c5e46611d2
pgolovko <pgolovko@golovko.org>
2026-09-22 18:03:34 -0700

    Change command namespace from '?' to '#'

--- muk_ai/static/src/core/commands/command_setup.js
+++ muk_ai/static/src/core/commands/command_setup.js
@@ -1,7 +1,7 @@
 import { _t } from '@web/core/l10n/translation';
 import { registry } from '@web/core/registry';
 
-registry.category('command_setup').add('?', {
+registry.category('command_setup').add('#', {
     debounceDelay: 200,
     name: _t('AI'),
     placeholder: _t('Search MuK AI chats and agents…'),
@@ -10,4 +10,4 @@ registry.category('command_setup').add('?', {
 
 registry
     .category('command_categories')
-    .add('muk_ai', { namespace: '?', name: _t('MuK AI') }, { sequence: 60 });
+    .add('muk_ai', { namespace: '#', name: _t('MuK AI') }, { sequence: 60 });
